3451e9e7d3111453e8598a5fbcaa656c1a5cf010
Gurgen Verdian <gurgen.verdian@gmail.com>
2022-02-27 10:03:05 -0500

    add Board() func

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ class TICTACTOE(tk.Tk):
         self.resizable(width=False, height=False) #disable resize window button
         self.Board() #create the game board
 
+    def Board(self):
+        for i in range(0, 3):
+            row = []
+            for j in range(0, 3):
+                row.append(tk.Button(self, width=10, height=3, font='Calibri 35 bold',
+                            command=lambda x=1, y=j: self.Turn_Taken(x, y)))
+                row[j].grid(row=i, column=j)
+            self.btns.append(row) #appending row to entire window
+        tk.Button(self, text='New Game', width=10, height=1, font='Calibri 15 bold',
+                  bg='black', fg='white', activebackground='blue3', activeforeground='white',
+                  command=self.NEWGAME).grid(row=3, column=1)
+
 
 
 TICTACTOE().mainloop()
